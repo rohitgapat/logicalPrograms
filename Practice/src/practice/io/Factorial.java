893ddae0d971bce67c44a763ca8a9f33678d9636
rohitgapat <rohitgapat233@gmail.com>
2025-06-23 14:31:44 +0530

    change in factorial

--- Practice/src/practice/io/Factorial.java
+++ Practice/src/practice/io/Factorial.java
@@ -1,10 +1,21 @@
 package practice.io;
 
 public class Factorial {
+	
+	public int findFactorial (int num) {
+		int x=1;
+		while(num!=0) {
+			x=x*num;
+			num--;
+		}
+		
+		return x;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Factorial f=new Factorial();
+		System.out.println(f.findFactorial(5));
+		
 		
 		
 	}
